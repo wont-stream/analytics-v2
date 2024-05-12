b15bb43ee7bd7282afeed2d2b7fa92d9b775b9b3
wont-stream <src@wont.stream>
2024-05-12 16:31:48 -0400

    upd

--- public/a.js
+++ public/a.js
@@ -1,142 +1,66 @@
-function DeviceMeta() {
-  let gl;
-  if (!gl) {
-    try {
-      let canvas = document.createElement("canvas");
-      gl =
-        canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
-    } catch (e) {}
-  }
-  let hints = {};
-  if (navigator.userAgentData) {
-    const fetchHints = [
-      "brands",
-      "mobile",
-      "platform",
-      "platformVersion",
-      "architecture",
-      "bitness",
-      "wow64",
-      "model",
-      "uaFullVersion",
-      "fullVersionList",
-    ];
+let g = document.createElement("canvas").getContext;
+const w = "webgl";
 
-    navigator.userAgentData.getHighEntropyValues(fetchHints).then((result) => {
-      hints = JSON.parse(JSON.stringify(result));
-    });
-  }
+let p = g(w) || g(`experimental-${w}`);
 
-  /** #################################
-   *  private helper methods
-   * ################################# */
+const n = navigator;
+const u = n.userAgentData;
 
-  /**
-   * get gpu name
-   * @returns {string|null}
-   */
-  function getGPUName() {
-    return gl
-      ? gl.getParameter(
-          gl.RENDERER
-        )
-      : null;
-  }
+let hints = {};
+if (u) {
+  const fetchHints = [
+    "brands",
+    "mobile",
+    "platform",
+    "platformVersion",
+    "architecture",
+    "bitness",
+    "wow64",
+    "model",
+    "uaFullVersion",
+    "fullVersionList",
+  ];
 
-  /**
-   * get device pixel ratio
-   */
-  function getRatio() {
-    return window.devicePixelRatio;
-  }
-
-  /**
-   * get device width of the screen in pixels
-   */
-  function getWidth() {
-    return window.screen.width * getRatio();
-  }
-
-  /**
-   * get device height of the screen in pixels
-   */
-  function getHeight() {
-    return window.screen.height * getRatio();
-  }
-  /**
-   * get check is ua apply
-   * @return {boolean}
-   */
-  function isAppleFamily() {
-    return /iPhone|iPad|Macintosh|Ipod/.exec(navigator.userAgent) !== null;
-  }
-
-  /**
-   * get timer stamp
-   * @return {DOMHighResTimeStamp|number}
-   */
-  function performance() {
-    return new Date().getTime();
-  }
-
-  /**
-   * get device memory
-   * @return {number|null}
-   */
-  function getDeviceMemory() {
-    return navigator.deviceMemory ? navigator.deviceMemory : null;
-  }
-
-  /**
-   * Determines if the query is supported by the device.
-   * @param {string} query
-   * @returns {boolean}
-   */
-  function hasMediaSupport(query) {
-    return window.matchMedia(query).matches;
-  }
-
-  function getMediaValue(name, values) {
-    for (let i = 0; i < values.length; i++) {
-      if (hasMediaSupport("(" + name + ": " + values[i] + ")")) {
-        return values[i];
-      }
-    }
-    return "";
-  }
-
-  function getMediaColorGamut() {
-    return getMediaValue("color-gamut", ["p3", "srgb"]);
-  }
-
-  /** #################################
-   *  public methods
-   * ################################# */
-
-  this.info = function () {
-    return {
-      useragent: navigator.userAgent,
-      meta: {
-        width: getWidth(),
-        height: getHeight(),
-        ratio: getRatio(),
-        ram: getDeviceMemory(),
-        gpu: getGPUName(),
-        colorDepth: screen.colorDepth,
-        gamut: getMediaColorGamut(),
-        cores: navigator.hardwareConcurrency || null,
-      },
-      hints: hints,
-    };
-  };
+  u.getHighEntropyValues(fetchHints).then((result) => {
+    hints = JSON.parse(JSON.stringify(result));
+  });
 }
 
-const data = new DeviceMeta().info()
+const r = window.devicePixelRatio;
+
+function hasMediaSupport(query) {
+  return window.matchMedia(query).matches;
+}
+
+function getMediaValue(name, values) {
+  for (let i = 0; i < values.length; i++) {
+    if (hasMediaSupport("(" + name + ": " + values[i] + ")")) {
+      return values[i];
+    }
+  }
+  return "";
+}
+
+function getMediaColorGamut() {
+  return getMediaValue("color-gamut", ["p3", "srgb", "rec2020"]);
+}
 
 fetch("/analyze", {
   method: "POST",
   headers: {
     "content-type": "application/json",
   },
-  body: JSON.stringify(data),
+  body: JSON.stringify({
+    meta: {
+      width: window.screen.width * r,
+      height: window.screen.height * r,
+      ratio: r,
+      ram: n.deviceMemory ? n.deviceMemory : null,
+      gpu: p.getParameter(p.RENDERER),
+      colorDepth: screen.colorDepth,
+      gamut: getMediaColorGamut(),
+      cores: n.hardwareConcurrency || null,
+    },
+    hints: hints,
+  }),
 });
